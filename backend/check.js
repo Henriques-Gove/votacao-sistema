be62f4f4ede77654f2e526ed499883a2662b135a
Henriques-Gove <henriquesjoaquimgove@gmail.com>
2026-06-06 02:40:03 +0200

    Remove temp files

--- backend/check.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-const sql = require('fs').readFileSync('src/config/schema.sql','utf8');
-const i = sql.indexOf('$2a');
-const h = sql.substring(i, i+60);
-console.log('Hash:', h);
-bcrypt.compare('Admin@123', h).then(r => console.log('Admin@123 match:', r));
-bcrypt.compare('password', h).then(r => console.log('password match:', r));
